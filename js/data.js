8afa5184c8b7783bf16cfe729aad83dc047b6f3e
pilketos <42822588+pilketos@users.noreply.github.com>
2018-08-31 20:03:11 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,2 +1,2 @@
 var kandidat = ['M Iqbal Maulana','David Augusta Chandra','Jirzis Wisam Muhammad'];
-var judul = "PEMILIHAN KETUA UMUM MPK<br>SMAN 1 JEMBER 2018-2019";
+var judul = "PEMILIHAN KETUA UMUM OSIS<br>SMAN 1 JEMBER 2018-2019";
